990dd2d1bad9bd3b5d4534b80cafd2d76d920d49
叶永平 <xieyongping@xieyongpingdeMacBook-Pro.local>
2024-10-29 14:42:48 +0800

    add llm unit test package, and add ollama client api

--- apps/blog/App.tsx
+++ apps/blog/App.tsx
@@ -6,15 +6,17 @@
  */
 
 import { helloworld } from '@yyp/shared';
-import React from 'react';
+import { codeGenerate, OllamaApiClient } from '@yyp/llm_unit_test';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  Text,
   useColorScheme,
   View,
-  Button
+  Button,
+  TextInput,
+  StyleSheet
 } from 'react-native';
 
 import {
@@ -25,6 +27,8 @@ import {
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [text, setText] = useState('');
+  const apiClient = new OllamaApiClient();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -37,14 +41,38 @@ function App(): React.JSX.Element {
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
+        keyboardDismissMode="interactive"
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <View style={{ alignSelf: 'center', alignItems: 'center', paddingTop: 100 }}>
-          <Text style={{backgroundColor: 'blue', color: 'red'}}>{'hello monorepo'}</Text>
+          <TextInput
+            style={styles.textInput}
+            // inputAccessoryViewID={inputAccessoryViewID}
+            onChangeText={setText}
+            value={text}
+            placeholder={'Please type here…'}
+          />
           <Button 
             title='call shared function'
             onPress={() => {
               helloworld()
+            }}
+          />
+           <Button 
+            title='GenCode'
+            onPress={async () => {
+              if (text.length <= 0 ) {
+                return
+              }
+              try {
+                const completion = await apiClient.generateCompletion({
+                  model: 'llama3.2',
+                  prompt: text
+                });
+                console.log('补全结果:', completion.response);
+              } catch (error: any) {
+                console.log(error)
+              }
             }}
           />
         </View>
@@ -52,5 +80,18 @@ function App(): React.JSX.Element {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  textInput: {
+    padding: 16,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
 
 export default App;
